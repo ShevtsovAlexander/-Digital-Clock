1f557e0b47b99dc5cc6774c1b9757f960f5e8b8b
ShevtsovAlexander <95102126+ShevtsovAlexander@users.noreply.github.com>
2023-01-25 22:25:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-
-
 function createDate() {
     var objDate = new Date();
     var day = objDate.getDay();
     var hour = objDate.getHours();
     var minute = objDate.getMinutes();
+    var sec = objDate.getSeconds()
     var ampm = hour >= 12 ? 'PM' : 'AM';
     var dayNames = ["SUN", "MON", "TUE", "WED", "THU","FRI", "SAT"];
 
@@ -12,13 +11,15 @@ function createDate() {
     hour = hour ? hour : '12';
     hour = hour < 10 ? '0' + hour : hour;
     minute = minute < 10 ? '0' + minute : minute;
+    sec = sec < 10 ? '0' + sec : sec;
 
 
     document.querySelector('#day').textContent = dayNames[day];
     document.querySelector('#hour').textContent = hour;
     document.querySelector('#minute').textContent = minute;
+    document.querySelector("#sec").textContent = sec;
     document.querySelector('#ampm').textContent = ampm;
 
-   setTimeout(createDate, 200);
+   setTimeout(createDate, 1000);
 }
 window.addEventListener('load', createDate)
